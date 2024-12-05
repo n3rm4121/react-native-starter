a6fdfacab7b46e648d8614431de27d528934d13f
n3rm4121 <nirmaldhakal2032@gmail.com>
2024-12-05 09:55:36 +0545

    Add Firebase integration and initial API endpoint; remove unused index file

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,15 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Button } from 'react-native';
+import { app } from '../../utils/firebaseConfig'
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { collection, getDocs } from "firebase/firestore"
 
-export default function profile() {
+
+export default function Profile() {
+
+
     return (
-        <View>
-            <Text>profile</Text>
-        </View>
+        <>
+
+        </>
     )
 }
